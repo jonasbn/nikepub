cc9c8086c8cfb0e2e2128a07ddf32898837a851d
jonasbn <jonasbn@logiclab.dk>
2010-02-01 22:19:47 +0000

    NIKEPUB-1 #time 10m #comment Added POD template and started filling in the details
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/NIKEPUB/trunk@6928 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/WWW/Nike/NikePlus/Public.pm
+++ lib/WWW/Nike/NikePlus/Public.pm
@@ -60,3 +60,97 @@ sub processor {
 1;
 
 __END__
+
+=pod
+
+=head1 NAME
+
+WWW::Nike::NikePlus::Public - retrieve data from Nike+ public interface
+
+=head1 SYNOPSIS
+
+    use WWW::Nike::NikePlus::Public;
+    
+    my $nike = WWW::Nike::NikePlus::Public->new({
+        userid  => 1755202461,
+        verbose => 1,
+    });
+    $xml = $nike->retrieve();
+        
+
+    my $nike = WWW::Nike::NikePlus::Public->new({});
+    $xml = $nike->retrieve({
+        userid => 1755202461,
+    });
+
+=head1 VERSION
+
+0.01
+
+=head1 DESCRIPTION
+
+=head1 SUBROUTINES/METHODS
+
+=head2 USAGE
+
+In order to make proper use of this class, you can either just use it as
+described in the L</SYNOPSIS> or you can subclass it and implement your own
+L</processor> method.
+
+=head1 DIAGNOSTICS
+
+=over
+
+=item * 
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES AND REQUIREMENTS
+
+=over
+
+=item * L<Carp>
+
+=item * L<WWW::Mechanize>
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 BUG REPORTING
+
+=head1 TEST AND QUALITY
+
+=head1 TODO
+
+=over
+
+=item *
+
+=back
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Carp>
+
+=item * L<WWW::Mechanize>
+
+=back
+
+=head1 AUTHOR
+
+=over
+
+=item Jonas B. Nielsen (jonasbn) C<< <jonasbn@cpan.org> >>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+=cut
