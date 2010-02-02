fca6c5ed88ef73992130106bb413d6f466af90ec
jonasbn <jonasbn@logiclab.dk>
2010-02-02 14:35:48 +0000

    Addressed [InputOutput::RequireBracedFileHandleWithPrint] violation
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/NIKEPUB/trunk@6936 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/WWW/Nike/NikePlus/Public.pm
+++ lib/WWW/Nike/NikePlus/Public.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use WWW::Mechanize;
 use Carp qw(croak);
-use Params::Validate qw(:all); #validate
+use Params::Validate qw(:all);    #validate
 use Data::Dumper;
 use English qw( -no_match_vars );
 
@@ -14,8 +14,8 @@ use base qw(Class::Accessor);
 __PACKAGE__->mk_accessors(qw(base_url verbose mech userid));
 
 use constant TRUE => 1;
-my $base_url =
-    'http://nikerunning.nike.com/nikeplus/v1/services/widget/get_public_run_list.jsp';
+my $base_url
+    = 'http://nikerunning.nike.com/nikeplus/v1/services/widget/get_public_run_list.jsp';
 
 use version; our $VERSION = qv('0.0.1');
 
@@ -25,38 +25,41 @@ sub new {
     my $self = bless {}, $class;
 
     $self->_validate_parameters($param);
-    
+
     my $agent = __PACKAGE__ . "-$VERSION";
-        
+
     if ( $param->{mech} ) {
-        $self->mech($param->{mech});
+        $self->mech( $param->{mech} );
     } else {
-        $self->mech(WWW::Mechanize->new( agent => $agent ));
+        $self->mech( WWW::Mechanize->new( agent => $agent ) );
     }
 
-    $self->base_url($param->{base_url});
-    
-    $self->verbose($param->{verbose});
-    $self->userid($param->{userid});
+    $self->base_url( $param->{base_url} );
+
+    $self->verbose( $param->{verbose} );
+    $self->userid( $param->{userid} );
 
     return $self;
 }
 
 sub _validate_parameters {
     my ( $self, $param ) = @_;
-    
-    my @parameters;    
-    while (my @pair = each %{$param}) {
+
+    my @parameters;
+    while ( my @pair = each %{$param} ) {
         push @parameters, @pair;
     }
 
-    validate( @parameters, {
-        mech => { can => [ 'get', 'content' ] , optional => TRUE },
-        verbose => { type => SCALAR, default => 0, optional => TRUE },
-        userid => { type => SCALAR, optional => TRUE },
-        base_url => { type => SCALAR, default => $base_url, optional => TRUE },
-    });
-    
+    validate(
+        @parameters,
+        {   mech => { can => [ 'get', 'content' ], optional => TRUE },
+            verbose => { type => SCALAR, default => 0, optional => TRUE },
+            userid => { type => SCALAR, optional => TRUE },
+            base_url =>
+                { type => SCALAR, default => $base_url, optional => TRUE },
+        }
+    );
+
     return 1;
 }
 
@@ -66,13 +69,13 @@ sub retrieve {
     $self->_validate_parameters($param);
 
     my $url = $self->base_url;
-    
+
     if ( defined $param->{userid} ) {
         $url = $self->base_url . $param->{userid};
     }
 
-    if (defined $param->{verbose}) {
-        $self->verbose($param->{verbose});
+    if ( defined $param->{verbose} ) {
+        $self->verbose( $param->{verbose} );
     }
 
     $self->mech->get($url)
@@ -87,7 +90,7 @@ sub processor {
     my ( $self, $content ) = @_;
 
     if ( $self->verbose ) {
-        print STDERR "Content: \n${$content}.\n";
+        print {*STDERR} "Content: \n${$content}.\n";
     }
 
     return $content;
