1371ae8e78781e8a34327556b3063411113c559b
jonasbn <jonasbn@logiclab.dk>
2011-04-08 19:30:33 +0000

    Removed use of version module, it was unnecessary
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/NIKEPUB/trunk@7552 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/WWW/Nike/NikePlus/Public.pm
+++ lib/WWW/Nike/NikePlus/Public.pm
@@ -16,7 +16,7 @@ use constant TRUE => 1;
 my $base_url
     = 'http://nikerunning.nike.com/nikeplus/v1/services/widget/get_public_run_list.jsp';
 
-use version; our $VERSION = qv('0.01');
+our $VERSION = '0.01';
 
 sub new {
     my ( $class, $param ) = @_;
